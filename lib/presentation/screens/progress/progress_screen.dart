93e2d4b4dea7c7995392b393fb688fd28de297e5
Joel Uzcategui <Joeluzcategui102@gmail.com>
2023-12-29 17:42:46 -0400

    Seccion 10 Linear y Circular progress controlados

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -9,6 +9,46 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Progress Indicators'),
+      ),
+      body: _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 30
+          ),
+          Text('Circular Progress Indicator'),
+          SizedBox(height: 10,),
+          CircularProgressIndicator( strokeWidth: 2, backgroundColor: Colors.black45,),
+          SizedBox(height: 20,),
+          Text('Circular Indicator Controlado'),
+          SizedBox(height: 10,),_ControllerProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
+
+
+class _ControllerProgressIndicator extends StatelessWidget {
+  const _ControllerProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
