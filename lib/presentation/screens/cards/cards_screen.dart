c8353e80b286d2ea3e04c685d5def936b2f29c26
Joel Uzcategui <Joeluzcategui102@gmail.com>
2023-12-29 14:53:17 -0400

    Seccion 9 Cards

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
+
+const cards = <Map<String,dynamic>>{
+
+  {'elevation': 0.0, 'label': 'elevation 0'},
+  {'elevation': 1.0, 'label': 'elevation 1'},
+  {'elevation': 2.0, 'label': 'elevation 2'},
+  {'elevation': 3.0, 'label': 'elevation 3'},
+  {'elevation': 4.0, 'label': 'elevation 4'},
+  {'elevation': 5.0, 'label': 'elevation 5'},
+  {'elevation': 6.0, 'label': 'elevation 6'},
+  {'elevation': 7.0, 'label': 'elevation 7'},
+  {'elevation': 8.0, 'label': 'elevation 8'},
+
+};
+
 class CardsScreen extends StatelessWidget {
 
   static const name = 'cards_screen';
@@ -12,7 +27,60 @@ class CardsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cards'),
       ),
-      body: const Placeholder(),
+      body: _CardsView(),
     );
+  }
+}
+
+class _CardsView extends StatelessWidget {
+  const _CardsView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...cards.map((card) => _CardType1(
+            elevation: card['elevation'],
+            label: card['label'],
+          )),
+        ],
+      ),
+    );
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+
+
+  final String label;
+  final double elevation;
+
+  const _CardType1({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(children: [
+          Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: (){}, 
+                icon: const Icon(Icons.more_vert_outlined)
+                ),
+            ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text(label),
+          )
+            ]
+          ),
+        ),  
+      );
   }
 }
