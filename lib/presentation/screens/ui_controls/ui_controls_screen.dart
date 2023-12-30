27c6eb3028a63c019f2550aa72192773780c62f6
Joel Uzcategui <Joeluzcategui102@gmail.com>
2023-12-29 23:05:00 -0400

    Seccion 10 Checkbox, Radios y otros Tiles

--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -9,6 +9,88 @@ class UiControlsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UI Controls'),
+      ),
+      body: _UiControlsView(),
+    );
+  }
+}
+
+class _UiControlsView extends StatefulWidget {
+  const _UiControlsView({
+    super.key,
+  });
+
+  @override
+  State<_UiControlsView> createState() => _UiControlsViewState();
+}
+
+enum Transportation { car, plane, boat, submarine }
+
+class _UiControlsViewState extends State<_UiControlsView> {
+
+  bool isDeveloper = true;
+  Transportation selectedTransportation = Transportation.car;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+
+        SwitchListTile(
+          title: const Text('Developer Mode'),
+          subtitle: const Text('Activar modo desarrollador'),
+          value: isDeveloper, 
+          onChanged: (value) => setState(() {
+            isDeveloper = !isDeveloper;
+            
+          })
+        ),
+
+        RadioListTile(
+          title: const Text('By Car'),
+          subtitle: const Text('Viajar en auto'),
+          value: Transportation.car, 
+          groupValue: selectedTransportation, 
+          onChanged: (value) => setState(() {
+            selectedTransportation = Transportation.car;
+          }),
+        ),
+        RadioListTile(
+          title: const Text('By Boat'),
+          subtitle: const Text('Viajar en barco'),
+          value: Transportation.boat, 
+          groupValue: selectedTransportation, 
+          onChanged: (value) => setState(() {
+            selectedTransportation = Transportation.boat;
+          }),
+        ),
+        RadioListTile(
+          title: const Text('By Plane'),
+          subtitle: const Text('Viajar en avion'),
+          value: Transportation.plane, 
+          groupValue: selectedTransportation, 
+          onChanged: (value) => setState(() {
+            selectedTransportation = Transportation.plane;
+          }),
+        ),
+        RadioListTile(
+          title: const Text('By Submarine'),
+          subtitle: const Text('Viajar en submarino'),
+          value: Transportation.submarine, 
+          groupValue: selectedTransportation, 
+          onChanged: (value) => setState(() {
+            selectedTransportation = Transportation.submarine;
+          }),
+        ),
+
+
+
+
+      ],
+    );
   }
 }
